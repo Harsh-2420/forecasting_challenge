abef5f3509454810bca1757004d1e65aa003c266
Harsh <harsh2420@gmail.com>
2020-02-08 19:27:53 -0500

    Pushed main.py

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import pandas as pd
-import numpy as np 
-import seaborn as sns
+import numpy as np
 import matplotlib.pyplot as plt
-# add the prediction model after further cleaning up
+
+# add the prediction model after further cleaning up`
